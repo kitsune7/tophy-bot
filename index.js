3a1f4a0cb899261639e5aa4db1c1e64eb0fbb2ae
Christopher Bradshaw <chris_bradshaw@databased.tech>
2020-05-18 03:25:33 -0600

    Separate messageActions from main file

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 'use strict'
 require('dotenv').config()
-const Discord = require('discord.js')
-const client = new Discord.Client()
 
+/* imports */
+const fs = require('fs')
+const Discord = require('discord.js')
+const responseFunctions = require('./responseFunctions.js')
+
+/* functions */
+const getMessageActions = () => {
+  return JSON.parse(fs.readFileSync('./messageActions.json'))
+}
+
+const checkFullMatch = (message, matchString) => {
+  return message.content.toLowerCase() === matchString
+}
+
+const checkFuzzyMatch = (message, matchString) => {
+  return message.content.toLowerCase().includes(matchString)
+}
+
+/* setup */
+const client = new Discord.Client()
+const messageActions = getMessageActions()
+
+/* events */
 client.on('ready', () => {
   console.log('I am ready!')
 })
 
 client.on('message', message => {
-  if (message.content === 'ping') {
-    message.channel.send('pong')
+  if (message.author.username === 'Tophy Bot') {
+    return
   }
+
+  messageActions.forEach(messageAction => {
+    const isMatch = messageAction.fullMessage ? checkFullMatch : checkFuzzyMatch
+
+    if (isMatch(message, messageAction.matchString)) {
+      if (messageAction.responseFunction) {
+        responseFunctions[messageAction.responseFunction](message)
+      } else {
+        message.channel.send(messageAction.simpleResponse)
+      }
+    }
+  })
 })
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
---
+++ responseFunctions.js
@@ -0,0 +1,17 @@
+const randomNumber = require('random-number-csprng')
+
+module.exports = {
+  rollD20: async message => {
+    const result = await randomNumber(1, 20)
+    message.channel.send(`You rolled a ${result}`)
+  },
+  goToSleep: message => {
+    message.channel.send('TophyBot supports your sleep. Get some rest. It\'s good for you. :gun:')
+  },
+  addFoxReaction: message => {
+    message.react('🦊')
+  },
+  addGunReaction: message => {
+    message.react('🔫')
+  }
+}
