1acf68d40dab4a0dd22e02d24831921d705537e3
Christopher Bradshaw <chris.kofox@gmail.com>
2021-11-04 22:32:51 -0600

    Move stop and play commands to message handlers

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -3,14 +3,12 @@ dotenv.config();
 
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
-import { loop, pause, play, queue, roll, skip, stop, volume } from './commands';
+import { roll } from './commands';
 
 const token = process.env.TOKEN as string;
 const clientId = process.env.CLIENT_ID as string;
 
-const jsonCommands = [loop, pause, play, queue, roll, skip, stop, volume].map(
-  (command) => command.toJSON()
-);
+const jsonCommands = [roll].map((command) => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(token);
 
--- src/interactions/router.ts
+++ src/interactions/router.ts
@@ -1,25 +1,12 @@
 import { CommandInteraction } from 'discord.js';
-import { DisTube } from 'distube';
-import {
-  loopResponse,
-  playResponse,
-  rollResponse,
-  stopResponse,
-} from '../commands';
+import { rollResponse } from '../commands';
 
 export const commandRouter = (
-  interaction: CommandInteraction,
-  distube: DisTube
+  interaction: CommandInteraction
 ): Promise<void> | void => {
   switch (interaction.commandName) {
     case 'roll':
       return rollResponse(interaction);
-    case 'play':
-      return playResponse(interaction, distube);
-    case 'stop':
-      return stopResponse(interaction, distube);
-    case 'loop':
-      return loopResponse(interaction, distube);
     default:
       return interaction.reply({
         content: 'Unknown command!',
