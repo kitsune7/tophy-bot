5816df866bc70dc6e85083ae747f0ea88e1a5831
Christopher Bradshaw <chris.kofox@gmail.com>
2023-07-22 18:57:11 -0600

    load commands

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import fs from 'fs';
     intents: [
       GatewayIntentBits.Guilds,
       GatewayIntentBits.GuildMessages,
-	  GatewayIntentBits.MessageContent,
+      GatewayIntentBits.MessageContent,
       GatewayIntentBits.GuildMessageReactions,
       GatewayIntentBits.GuildVoiceStates,
     ],
@@ -26,9 +26,22 @@ import fs from 'fs';
 
   const token = process.env.TOKEN as string;
   const clientId = process.env.CLIENT_ID as string;
-  
-  client.commands = new Collection()
-  client.aliases = new Collection()
+
+  client.commands = new Discord.Collection();
+  client.aliases = new Discord.Collection();
+
+  fs.readdir('./commands/', (err, files) => {
+    if (err) return console.log('Could not find any commands!');
+    const jsFiles = files.filter((f) => f.split('.').pop() === 'js');
+    if (jsFiles.length <= 0) return console.log('Could not find any commands!');
+    jsFiles.forEach((file) => {
+      const cmd = require(`./commands/${file}`);
+      console.log(`Loaded ${file}`);
+      client.commands.set(cmd.name, cmd);
+      if (cmd.aliases)
+        cmd.aliases.forEach((alias) => client.aliases.set(alias, cmd.name));
+    });
+  });
 
   client.once('ready', () => {
     console.log('Ready!');
@@ -36,14 +49,16 @@ import fs from 'fs';
 
   client.on('error', console.error);
   client.on('warn', console.warn);
-  
-  client.on('messageCreate', async message => {
+
+  client.on('messageCreate', async (message) => {
     if (message.author.bot || !message.guild) return;
-    const prefix = "!";
+    const prefix = '!';
     if (!message.content.startsWith(prefix)) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-    const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+    const cmd =
+      client.commands.get(command) ||
+      client.commands.get(client.aliases.get(command));
     if (!cmd) return;
     if (cmd.inVoiceChannel && !message.member.voice.channel) {
       return message.channel.send(`You must be in a voice channel!`);
@@ -56,39 +71,48 @@ import fs from 'fs';
     }
   });
 
-  const status = queue =>
-  `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.names.join(', ') || 'Off'}\` | Loop: \`${
-    queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
-  }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
-client.distube
-  .on('playSong', (queue, song) =>
-    queue.textChannel.send(
-      `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${
-        song.user
-      }\n${status(queue)}`
+  const status = (queue) =>
+    `Volume: \`${queue.volume}%\` | Filter: \`${
+      queue.filters.names.join(', ') || 'Off'
+    }\` | Loop: \`${
+      queue.repeatMode
+        ? queue.repeatMode === 2
+          ? 'All Queue'
+          : 'This Song'
+        : 'Off'
+    }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``;
+  client.distube
+    .on('playSong', (queue, song) =>
+      queue.textChannel.send(
+        `Playing \`${song.name}\` - \`${
+          song.formattedDuration
+        }\`\nRequested by: ${song.user}\n${status(queue)}`
+      )
     )
-  )
-  .on('addSong', (queue, song) =>
-    queue.textChannel.send(
-      `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
-    );
-  )
-  .on('addList', (queue, playlist) =>
-    queue.textChannel.send(
-      `Added \`${playlist.name}\` playlist (${
-        playlist.songs.length
-      } songs) to queue\n${status(queue)}`
-    );
-  )
-  .on('error', (channel, e) => {
-    if (channel) channel.send(`An error encountered: ${e.toString().slice(0, 1974)}`);
-    else console.error(e);
-  })
-  .on('empty', channel => channel.send('Voice channel is empty! Leaving the channel...'))
-  .on('searchNoResult', (message, query) =>
-    message.channel.send(`No result found for \`${query}\`!`)
-  )
-  .on('finish', queue => queue.textChannel.send('Finished!'))
-  
+    .on('addSong', (queue, song) =>
+      queue.textChannel.send(
+        `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+      )
+    )
+    .on('addList', (queue, playlist) =>
+      queue.textChannel.send(
+        `Added \`${playlist.name}\` playlist (${
+          playlist.songs.length
+        } songs) to queue\n${status(queue)}`
+      )
+    )
+    .on('error', (channel, e) => {
+      if (channel)
+        channel.send(`An error encountered: ${e.toString().slice(0, 1974)}`);
+      else console.error(e);
+    })
+    .on('empty', (channel) =>
+      channel.send('Voice channel is empty! Leaving the channel...')
+    )
+    .on('searchNoResult', (message, query) =>
+      message.channel.send(`No result found for \`${query}\`!`)
+    )
+    .on('finish', (queue) => queue.textChannel.send('Finished!'));
+
   return client.login(process.env.TOKEN);
 })();
