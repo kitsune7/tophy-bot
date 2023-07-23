0a76d3e56df6f135e636541571a78fcb452d1d80
Christopher Bradshaw <chris.kofox@gmail.com>
2023-07-22 18:48:42 -0600

    use example for youtube player bot

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,34 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { Client, GatewayIntentBits } from 'discord.js';
+import Discord, { Client, GatewayIntentBits } from 'discord.js';
 import { DisTube } from 'distube';
 import { YtDlpPlugin } from '@distube/yt-dlp';
-
-import { runMessageAction } from './message-actions';
-// import { commandRouter } from './interactions';
-// import { roll } from './commands';
+import fs from 'fs';
 
 (async function main() {
   const client = new Client({
     intents: [
       GatewayIntentBits.Guilds,
       GatewayIntentBits.GuildMessages,
+	  GatewayIntentBits.MessageContent,
       GatewayIntentBits.GuildMessageReactions,
       GatewayIntentBits.GuildVoiceStates,
     ],
   });
   const distube = new DisTube(client, {
-    leaveOnFinish: true,
-    leaveOnEmpty: true,
-    leaveOnStop: true,
+    leaveOnStop: false,
+    emitNewSongOnly: true,
+    emitAddSongWhenCreatingQueue: false,
+    emitAddListWhenCreatingQueue: false,
     plugins: [new YtDlpPlugin()],
-    searchSongs: 1,
   });
 
   const token = process.env.TOKEN as string;
   const clientId = process.env.CLIENT_ID as string;
-  // const jsonCommands = [roll].map((command) => command.toJSON());
-  // const rest = new REST({ version: '9' }).setToken(token);
+  
+  client.commands = new Collection()
+  client.aliases = new Collection()
 
   client.once('ready', () => {
     console.log('Ready!');
@@ -37,138 +36,59 @@ import { runMessageAction } from './message-actions';
 
   client.on('error', console.error);
   client.on('warn', console.warn);
-
-  distube
-    .on('searchResult', (message, result) => {
-      let i = 0;
-      message.channel.send(
-        `**Choose an option from below**\n${result
-          .map(
-            (song) => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``
-          )
-          .join('\n')}\n*Enter anything else or wait 30 seconds to cancel*`
-      );
-    })
-    .on('error', (textChannel, error) => {
-      console.error(error);
-      textChannel.send(
-        `An error encountered: ${error.toString().slice(0, 2000)}`
-      );
-    });
-
-  client.on('messageCreate', async (message) => {
+  
+  client.on('messageCreate', async message => {
     if (message.author.bot || !message.guild) return;
-
-    const commandPrefix = '!';
-    if (message.content.startsWith(commandPrefix)) {
-      const args = message.content
-        .slice(commandPrefix.length)
-        .trim()
-        .split(/ +/g);
-      const command = args.shift();
-      switch (command) {
-        // case 'deploy':
-        //   if (message.author.id === process.env.TOPHY_USER_ID) {
-        //     await rest
-        //       .put(Routes.applicationCommands(clientId), { body: {} })
-        //       .then(() =>
-        //         console.log('Successfully registered application commands.')
-        //       )
-        //       .catch(console.error);
-        //     await message.reply('Deployed!');
-        //   }
-        //   break;
-
-        case 'play':
-          await distube.play(message.member.voice.channel, args.join(' '), {
-			message,
-			textChannel: message.channel,
-			member: message.member
-          }).catch(error => message.reply(error.message));
-          break;
-
-        case 'stop':
-          await distube.stop(message);
-          message.channel.send('Stopped the queue!');
-          break;
-
-        case 'loop':
-          const mode = distube.setRepeatMode(message);
-          const modeToDescription = {
-            0: 'off',
-            1: 'song',
-            2: 'queue',
-          };
-          message.channel.send(
-            `Set repeat mode to ${
-              modeToDescription?.[mode as 0 | 1 | 2] ??
-              "sad fox. You're not valid."
-            }`
-          );
-          break;
-
-        case 'resume':
-          distube.resume(message);
-          break;
-
-        case 'pause':
-          distube.pause(message);
-          break;
-
-        case 'skip':
-          await distube.skip(message);
-          break;
-
-        case 'seek':
-          const secondsPerMinute = 60;
-          const getTotalSecondsFromVideoTime = () => {
-            const seekTime = args?.[0];
-            const videoTimeRegex = /^([0-9]+:[0-9]{1,2}|[0-9]{1,2}):[0-9]{2}$/;
-
-            if (!videoTimeRegex.test(seekTime)) {
-              return 0;
-            }
-
-            const times = seekTime.split(':').map((num) => Number(num));
-            const timeMultiplier = times.length - 1;
-            return times.reduce(
-              (totalSeconds, time, i) =>
-                totalSeconds + time * secondsPerMinute ** (timeMultiplier - i),
-              0
-            );
-          };
-          await distube.seek(message, getTotalSecondsFromVideoTime());
-          break;
-
-        case 'queue':
-          const queue = distube.getQueue(message);
-          if (!queue) {
-            message.channel.send('Nothing playing right now!');
-          } else {
-            message.channel.send(
-              `Current queue:\n${queue.songs
-                .map(
-                  (song, id) =>
-                    `**${id ? id : 'Playing'}**. ${song.name} - \`${
-                      song.formattedDuration
-                    }\``
-                )
-                .slice(0, 10)
-                .join('\n')}`
-            );
-          }
-          break;
-      }
+    const prefix = "!";
+    if (!message.content.startsWith(prefix)) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+    if (!cmd) return;
+    if (cmd.inVoiceChannel && !message.member.voice.channel) {
+      return message.channel.send(`You must be in a voice channel!`);
     }
-
-    runMessageAction(message);
+    try {
+      cmd.run(client, message, args);
+    } catch (e) {
+      console.error(e);
+      message.channel.send(`Error: \`${e}\``);
+    }
   });
 
-  /*client.on('interactionCreate', async (interaction) => {
-    if (!interaction.isCommand()) return;
-    await commandRouter(interaction);
-  });*/
-
-  // Log our bot in using the token from https://discordapp.com/developers/applications/me
+  const status = queue =>
+  `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.names.join(', ') || 'Off'}\` | Loop: \`${
+    queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
+  }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
+client.distube
+  .on('playSong', (queue, song) =>
+    queue.textChannel.send(
+      `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${
+        song.user
+      }\n${status(queue)}`
+    )
+  )
+  .on('addSong', (queue, song) =>
+    queue.textChannel.send(
+      `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+    );
+  )
+  .on('addList', (queue, playlist) =>
+    queue.textChannel.send(
+      `Added \`${playlist.name}\` playlist (${
+        playlist.songs.length
+      } songs) to queue\n${status(queue)}`
+    );
+  )
+  .on('error', (channel, e) => {
+    if (channel) channel.send(`An error encountered: ${e.toString().slice(0, 1974)}`);
+    else console.error(e);
+  })
+  .on('empty', channel => channel.send('Voice channel is empty! Leaving the channel...'))
+  .on('searchNoResult', (message, query) =>
+    message.channel.send(`No result found for \`${query}\`!`)
+  )
+  .on('finish', queue => queue.textChannel.send('Finished!'))
+  
   return client.login(process.env.TOKEN);
 })();
